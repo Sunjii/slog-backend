68db796c91a1a8a97928e61ffdee79bcd3707ce5
Sunjii <sophieadenn@gmail.com>
2022-12-15 15:22:00 +0900

    remove useless user.resolver

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -2,6 +2,7 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { CreateUserInput, CreateUserOutput } from './dto/create-user.dto';
 import { UserEntity } from './entities/user.entity';
+import { LoginInput, LoginOutput } from './dto/login.dto';
 
 @Resolver((of) => UserEntity)
 export class UsersResolver {
@@ -13,13 +14,13 @@ export class UsersResolver {
   }
 
   @Query((returns) => UserEntity)
-  findAll() {
-    return this.usersService.findAll();
-  }
-
-  @Query((returns) => UserEntity)
   findOne(@Args('id') id: number) {
     return this.usersService.findOne(id);
+  }
+
+  @Mutation((returns) => LoginOutput)
+  async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
+    return this.usersService.login(loginInput);
   }
 
   // @Mutation('updateUser')
