85a7e24690321efeb772063b3ffe0105fe8c3bde
Sunjii <sophieadenn@gmail.com>
2022-12-19 13:43:53 +0900

    Post Resover & Service (create, read one, read all) add

--- src/post/post.resolver.ts
+++ src/post/post.resolver.ts
@@ -1,25 +1,36 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { PostService } from './post.service';
 import { PostEntity } from './entities/post.entity';
-import { CreatePostInput } from './dto/create-post.input';
+import { CreatePostInput, CreatePostOutput } from './dto/create-post.input';
 import { UpdatePostInput } from './dto/update-post.input';
+import { PostOutput } from './dto/post-output.dto';
+import { AllPostsInput, AllPostsOutput } from './dto/all-posts.dto';
+import { UserEntity } from 'src/users/entities/user.entity';
+import { AuthUser } from 'src/auth/auth-user.decorator';
 
 @Resolver((of) => PostEntity)
 export class PostResolver {
   constructor(private readonly postService: PostService) {}
 
-  @Mutation((returns) => PostEntity)
-  createPost(@Args('createPostInput') createPostInput: CreatePostInput) {
-    return this.postService.create(createPostInput);
+  @Mutation((returns) => CreatePostOutput)
+  async createPost(
+    @AuthUser() authUser: UserEntity,
+    @Args('createPostInput') createPostInput: CreatePostInput,
+  ): Promise<CreatePostOutput> {
+    return this.postService.create(authUser, createPostInput);
   }
 
-  @Query((returns) => [PostEntity])
-  findAllPosts() {
-    return this.postService.findAll();
+  @Query((returns) => AllPostsOutput)
+  findAllPosts(
+    @Args('input') postsInput: AllPostsInput,
+  ): Promise<AllPostsOutput> {
+    return this.postService.findAll(postsInput);
   }
 
-  @Query((returns) => PostEntity)
-  findPostByID(@Args('id', { type: () => Int }) id: number) {
+  @Query((returns) => PostOutput)
+  findPostByID(
+    @Args('id', { type: () => Int }) id: number,
+  ): Promise<PostOutput> {
     return this.postService.findOne(id);
   }
 
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
+import { AllPostsInput, AllPostsOutput } from './dto/all-posts.dto';
 import { CreatePostInput } from './dto/create-post.input';
+import { PostOutput } from './dto/post-output.dto';
 import { UpdatePostInput } from './dto/update-post.input';
 import { PostEntity } from './entities/post.entity';
 
+const PAGENATION = 5;
 @Injectable()
 export class PostService {
   constructor(
@@ -12,21 +16,65 @@ export class PostService {
     private readonly posts: Repository<PostEntity>,
   ) {}
 
-  create(createPostInput: CreatePostInput) {
-    return 'This action adds a new post';
+  async create(user: UserEntity, createPostInput: CreatePostInput) {
+    try {
+      console.log(user);
+      if (user === undefined) {
+        throw new Error('작성자 정보가 없습니다');
+      }
+      const newPost = this.posts.create(createPostInput);
+      newPost.user = user;
+
+      await this.posts.save(newPost);
+      return {
+        ok: true,
+      };
+    } catch (e) {
+      return { ok: false, error: `포스트 작성 실패 ${e}` };
+    }
   }
 
-  findAll() {
-    return `This action returns all post`;
+  async findAll({ page }: AllPostsInput): Promise<AllPostsOutput> {
+    try {
+      const [posts, totalResults] = await this.posts.findAndCount({
+        skip: (page - 1) * PAGENATION,
+        take: PAGENATION,
+        order: {
+          createAt: 'DESC',
+        },
+      });
+
+      if (posts.length === 0) {
+        throw new Error('');
+      }
+
+      return {
+        ok: true,
+        posts,
+        totalPage: Math.ceil(totalResults / PAGENATION),
+        totalResults,
+      };
+    } catch (e) {
+      return {
+        ok: false,
+        error: '포스트가 하나도 없습니다.',
+      };
+    }
   }
 
-  async findOne(id: number) {
-    const post = await this.posts.findOneBy({ id });
-
-    return {
-      ok: true,
-      post,
-    };
+  async findOne(id: number): Promise<PostOutput> {
+    try {
+      const post = await this.posts.findOneOrFail({ where: { id } });
+      return {
+        ok: true,
+        post,
+      };
+    } catch (e) {
+      return {
+        ok: false,
+        error: `${id} 포스트를 찾을 수 없습니다.`,
+      };
+    }
   }
 
   update(id: number, updatePostInput: UpdatePostInput) {
